6b4c6c2cda9dbc0561d84c256f48f66f16bd590e
shokhDeveloper <shohijahonmusinkulov@gmail.com>
2026-06-11 19:05:27 +0500

    Last my  commit

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log('RUN')
+
+// U -> Untracked (hali githubda mavjud emas)
+// M -> Modified (githubda bor faqat ma'lum bir o'zgarishi yo'q)
+// A -> Added (Stage areaga olinganda ya'ni add qilganimizda)
+// R -> Renamed (Nomi tahrirlangan filening qayta add qilingan holati)
