31af45069c0d790ce7e16a0994011f2e3d4ec8df
shokhDeveloper <shohijahonmusinkulov@gmail.com>
2026-06-12 10:37:20 +0500

    Add js code

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ console.log('RUN')
 // M -> Modified (githubda bor faqat ma'lum bir o'zgarishi yo'q)
 // A -> Added (Stage areaga olinganda ya'ni add qilganimizda)
 // R -> Renamed (Nomi tahrirlangan filening qayta add qilingan holati)
+
+console.log('Advanced git')
